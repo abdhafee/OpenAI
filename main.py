bf68b01125aff77c32eb76b41ce1445773ad9884
abdhafeel <abdhafee@icloud.com>
2025-04-20 09:51:53 +0530

    Implemented AI chatbot using openrouter

---
+++ main.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+import os
+ 
+api_key =os.getenv("API_KEY")
+
+if not api_key:
+    raise ValueError("API_KEY NOT FOUND")
+ 
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=api_key
+)
+chat_history = []
+personas = {
+        "default": "You are a helpful AI Assistant",
+        "sarcastic": "Your are a sarcastic AI who gives witty and mocking responses",
+        "poet": "You are a poetic AI that responds in rhymes and verses"
+}
+
+print("Choose a persona : (defualt / sarcastic / poet)")
+
+user_persona_input = input("Enter Persona :").strip().lower()
+
+persona = personas.get(user_persona_input, personas["poet"])
+
+chat_history.append({"role": "system", "content": personas["poet"]})
+
+while True:
+ 
+    user_input = input("Enter Your Prompt:")
+    
+    chat_history.append({"role": "user", "content": user_input})
+    if user_input == "exit":
+        break
+ 
+    completion = client.chat.completions.create(
+ 
+    model="deepseek/deepseek-r1-zero:free",
+    messages=chat_history
+    )
+    response = completion.choices[0].message.content
+    print(response)
+
+    chat_history.append({"role": "user", "content": user_input})
